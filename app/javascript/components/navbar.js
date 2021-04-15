56e766c97d1d6ad339c585ac1fe484e2c700a25a
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-04-15 10:11:50 +0200

    Lecture

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,18 @@
+const initUpdateNavbarOnScroll = () => {
+  const navbar = document.querySelector('.navbar-lewagon');
+  if (navbar) {
+    window.addEventListener('scroll', () => {
+      console.log('window.scrollY');
+      console.log(window.scrollY);
+      console.log('window.innerHeight');
+      console.log(window.innerHeight);
+      if (window.scrollY >= window.innerHeight) {
+        navbar.classList.add('navbar-lewagon-white');
+      } else {
+        navbar.classList.remove('navbar-lewagon-white');
+      }
+    });
+  }
+}
+
+export { initUpdateNavbarOnScroll };
